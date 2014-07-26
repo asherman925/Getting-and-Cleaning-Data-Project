f5eed222a5b668f85caaa3148f7464e828cacd95
Andy Sherman <asherman925@gmail.com>
2014-07-26 09:34:12 -0700

    renames columns in tidy data set

--- run_analysis.R
+++ run_analysis.R
@@ -50,4 +50,5 @@ names(mean_std_set)[2]<-"Activity";
 var_cols<-names(mean_std_set)[c(3:68)];
 melt_mean_std_set <-melt(mean_std_set, id=c("Subject", "Activity"), measure.vars=var_cols);
 tidy<-dcast(melt_mean_std_set, Subject + Activity ~ variable, mean);
+names(tidy)<-gsub("Time", "mean.Time", names(tidy));
 write.table(tidy,file="tidy.txt");
